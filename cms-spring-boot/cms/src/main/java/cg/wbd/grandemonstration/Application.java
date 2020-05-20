d02d338ea3490570c852cfef4c7a7d464dbcdb06
keanh <keanh275@gmail.com>
2020-05-20 14:57:24 +0700

    add

---
+++ cms-spring-boot/cms/src/main/java/cg/wbd/grandemonstration/Application.java
@@ -0,0 +1,19 @@
+package cg.wbd.grandemonstration;
+
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    @Bean
+    public CustomerService customerService(){
+        return new SimpleCustomerServiceImpl();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
